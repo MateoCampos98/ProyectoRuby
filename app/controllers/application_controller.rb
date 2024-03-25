585c9093597664c1ef850a75447995b7abdc77de
Mateo Campos <campos129823@gmail.com>
2024-03-25 16:27:06 -0500

    Callbackas y gema cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
 
+    rescue_from CanCan::AccessDenied do |execption|
+      redirect_to root_path
+    end
+
     def set_locale
         I18n.locale = 'es'
-      end
+    end
 end
